22526b0708dad92964b34f1534de0dac1957f4bd
= <=>
2020-10-13 16:14:13 -0700

    finished 3 pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import WelcomeForm from "../comps/WelcomeForm";
-import {HeroImageOne} from "../stories/HeroComp.stories.js";
+import { HeroImageOne, HeroImageTwo } from "../stories/HeroComp.stories.js";
 import SignupForm from "../comps/SignupForm";
 import Navigation from "../comps/Navigation";
 import MoreButtons from "../comps/MoreButtons";
@@ -11,12 +11,38 @@ import Content from "../comps/Content";
 
 
 export default function Home() {
-  return <div className="home">
-    <div className="hero">
-      <HeroImageOne/>
+  return <div className="app">
+
+    <div className="home1">
+      <div className="hero">
+        <HeroImageOne />
+        <div className="form1"><WelcomeForm /></div>
+      </div>
     </div>
-    <div className="form">
-      <WelcomeForm />
+
+    <div className="home2">
+      <div className="form2"><SignupForm /></div>
+      <div className="hero2"><HeroImageTwo /></div>
     </div>
+
+    <div className="home3">
+      <div className="nav"><Navigation /></div>
+      <div>
+        <div className="left">
+          <div><MoreButtons background="#3D6FF1" color="FFF" text="Start Creating"/></div>
+        </div>
+        <div className="right">
+          <div className="msg"><WelcomeMsg /></div>
+          <div>Nothing Here yet</div>
+          <div className="tutor"><Tutorial /></div>
+        </div>
+      </div>
+
+
+    </div>
+
   </div>
+
+
+
 }
